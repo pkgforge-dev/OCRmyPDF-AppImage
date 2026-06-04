6b454314a8667aabbd05408093444cbaf2db0b30
Samueru-sama <xdglawyer@outlook.com>
2026-06-03 22:46:44 -0400

    package app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,9 +13,22 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package ocrmypdf
 
 # If the application needs to be manually built that has to be done down here
+
+# Use tessdata_fast script models since they are way lighter than what archlinux provides
+echo "Downloading tesseract script models..."
+echo "---------------------------------------------------------------"
+tessdata_dir=/usr/share/tessdata
+tessdata_source=https://github.com/tesseract-ocr/tessdata_fast/raw/main
+rm -rf "$tessdata_dir"
+mkdir -p "$tessdata_dir"
+for lang in deu eng fin fra por rus spa; do
+	echo "Downloading $lang.traineddata"
+	wget --retry-connrefused --tries=30 "$tessdata_source"/"$lang".traineddata -O "$tessdata_dir"/"$lang".traineddata
+done
+wget --retry-connrefused --tries=30 "$tessdata_source"/osd.traineddata -O "$tessdata_dir"/osd.traineddata
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
 #
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ocrmypdf | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://raw.githubusercontent.com/ocrmypdf/OCRmyPDF/refs/heads/main/docs/images/logo-square.png
+export DESKTOP=DUMMY
+export DEPLOY_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/ocrmypdf
 
 # Additional changes can be done in between here
 
@@ -21,4 +22,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
