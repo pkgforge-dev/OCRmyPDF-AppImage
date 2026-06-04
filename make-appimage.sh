70f586877be799ec8fef3c24320c2067f69e3595
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-06-03 22:55:22 -0400

    set `MAIN_BIN`

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=https://raw.githubusercontent.com/ocrmypdf/OCRmyPDF/refs/heads/main/docs/images/logo-square.png
 export DESKTOP=DUMMY
 export DEPLOY_PYTHON=1
+export MAIN_BIN=ocrmypdf
 
 # Deploy dependencies
 quick-sharun /usr/bin/ocrmypdf
